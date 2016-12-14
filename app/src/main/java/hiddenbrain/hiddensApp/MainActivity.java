59243dc42e646b202dcca381c85ad01169ba16ce
hiddensbrain <hiddensmaster@gmail.com>
2016-12-14 21:53:52 +0530

    add menu

--- app/src/main/java/hiddenbrain/hiddensApp/MainActivity.java
+++ app/src/main/java/hiddenbrain/hiddensApp/MainActivity.java
@@ -3,11 +3,19 @@ package hiddenbrain.hiddensApp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onAboutButtonClick(View view) {
+        Intent intentAbout = new Intent(this, AboutActivity.class);
+        startActivity(intentAbout);
+    }
+
+    public void onMaimMenuClick(MenuItem item) {
         Intent intentAbout = new Intent(this, AboutActivity.class);
         startActivity(intentAbout);
     }
